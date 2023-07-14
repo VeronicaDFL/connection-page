b1faca4f4cd5d12eea296d712b84219771d12c48
Veronica De Felice <verodefelic@gmail.com>
2023-07-14 16:54:10 -0400

    mvp met

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -12,8 +12,14 @@ export default function Hero () {
             </h1>
             <p className="mb-8 leading-relaxed">I am a creative and resourceful software engineer with a successful track record of transitioning between different industries and roles. I am passionate about helping my clients achieve their goals, and I take pride in my work. Outside of work, I enjoy spending time outdoors and pursuing creative interests. I am excited about the future of software engineering, and I am confident that I can make a significant contribution to the field.</p>
             <div className="flex justify-center">
-              <button className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">Button</button>
-              <button className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">Button</button>
+              <button className="inline-flex text-white bg-[#701a75] border-0 py-2 px-6 focus:outline-none hover:bg-[#c026d3] rounded text-lg"><a href="https://www.bocacode.com/candidates/software-engineer/veronica-de-felice" rel="noopener noreferrer" target="_blank">Profile</a></button>
+              <button className="ml-4 inline-flex text-white bg-[#701a75] border-0 py-2 px-6 focus:outline-none hover:bg-[#c026d3] rounded text-lg"><a href="/images/Veronica_DeFelice_Resume.pdf" rel="noopener noreferrer" target="_blank">Resume</a></button>
+            </div>
+            <div className="mt-12">
+            <a href="https://github.com/VeronicaDFL" className="ml-3 text-titanium-500 inline-flex" target="_blank" rel="noreferrer"><img src="/images/github.svg"height="30" width="30" alt="link to project" /></a>
+            <a href="https://www.linkedin.com/in/veronicadefelice/" className="ml-3 text-titanium-500 inline-flex" target="_blank" rel="noreferrer"><img src="/images/link.png"height="30" width="30" alt="link to project" /></a>
+            <a href="https://main.d3qkj62q5kdmfa.amplifyapp.com/" className="ml-3 text-titanium-500 inline-flex" target="_blank" rel="noreferrer"><img src="/images/connect.png"height="30" width="30" alt="link to project" /></a>
+            <a href="mailto:verodefelic@gmail.com" className="ml-3 text-titanium-500 inline-flex" target="_blank" rel="noreferrer"><img src="/images/email.png"height="30" width="30" alt="link to project" /></a>
             </div>
           </div>
         </div>
