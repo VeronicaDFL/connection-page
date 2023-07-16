8bcc550f79a2b53249fb1185a949a905c13de910
Veronica De Felice <verodefelic@gmail.com>
2023-07-16 12:39:15 -0400

    added box-shadow profile pic

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,7 +4,7 @@ export default function Hero () {
         <section className=" mb-0 text-gray-400 bg-gray-900 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 md:mb-0 mb-10">
-            <img className="scale-90 hover:scale-100 hover:shadow-2xl transition ease-in-out duration-500 object-cover object-center rounded" alt="hero" src="/images/vero.jpg"/>
+            <img className="shadow-[0px_10px_100px_10px] shadow-center shadow-[#701a75] scale-90 hover:scale-100 hover:shadow-2xl transition ease-in-out duration-500 object-cover object-center rounded" alt="hero" src="/images/vero.jpg"/>
           </div>
           <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">VERONICA DE FELICE
