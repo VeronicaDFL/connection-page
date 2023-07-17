130f72dac7c1f49c0cbc720b446ab16d6280c25b
Veronica De Felice <verodefelic@gmail.com>
2023-07-17 15:07:59 -0400

    modified h in hero

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Veronica De Felice: Connection Hub',
   description: 'Connecting Links related to my profile',
+  image: '/favicon.ico'
 }
 
 export default function RootLayout({ children }) {
