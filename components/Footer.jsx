6aed8e6d03400899224014e85d1601c75f534c09
Veronica De Felice <verodefelic@gmail.com>
2023-07-18 23:42:13 -0400

    Updated Footer text-center

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,13 +2,10 @@ export default function Footer () {
 
     return (
     <footer className="text-center flex items-center justify-center w-full  fixed bottom-0 text-gray-400 bg-gray-900 body-font">
-    <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-      
-      <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">© 2023 Veronica De Felice
-        
+     <div className="container justify-center px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+      <p className=" text-center text-gray-400 ">© 2023 Veronica De Felice
       </p>
-     
-    </div>
+     </div>
   </footer>
     )
 }
